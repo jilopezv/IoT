b312bcc5eb1647ff63e24fcd5fad01dcecd4235c
Danny <danny@pop-os.localdomain>
2019-11-01 13:40:54 -0500

    Modifying model for things interaction

---
+++ gateway-rasp/Device.py
@@ -0,0 +1,105 @@
+import Home
+from abc import ABC, abstractmethod
+
+
+class Device(ABC):
+    """ Class Device
+        
+        Attributes:    
+    """
+    home = Home
+
+    def __init__(self, id, status, connectionStatus, name, home):
+        self.id = id
+        self.status = status
+        self.connectionStatus = connectionStatus
+        self.name = name
+        self.home = home
+        self.msg_count = 0
+        super().__init__()
+
+    @abstractmethod
+    def process_external_msg(self, parameter_list):
+        pass
+
+    # TODO: refactor this method. opt1: create a concrete method with current implementation
+    def process_internal_msg(self, parameter_list):
+        self.msg_count += 1
+        # TODO: change explicit number 5 to a constant variable
+        if self.msg_count == 5:
+            self.home.send_alive_msg(id)
+            self.msg_count = 0
+
+    def clean_count(self):
+        self.msg_count = 0
+
+    @abstractmethod
+    def get_json(self, parameter_list):
+        pass
+
+
+class Light(Device):
+    def __init__(self, id, status, connectionStatus, name, home):
+        super().__init__(id, status, connectionStatus, name, home)
+
+    def process_external_msg(self, parameter_list):
+        raise NotImplementedError
+
+    def process_internal_msg(self, payload):
+        print(payload)
+        if payload != "ONLINE":
+            self.toggle_light()
+            self.clean_count()
+        super().process_internal_msg()
+
+    # raise NotImplementedError
+    def toggle_light(self):
+        if self.status == "ON":
+            self.status = "OFF"
+        else:
+            self.status = "ON"
+        self.home.send_msg_2_server(self.status)
+
+
+class Camera(Device):
+    def process_external_msg(self, parameter_list):
+        raise NotImplementedError
+
+    def process_internal_msg(self, payload):
+        print(payload)
+        if self.status != payload:
+            self.status = payload
+            self.home.send_msg_2_server(payload)
+            self.clean_count()
+        super().process_internal_msg()
+
+
+class Temperature(Device):
+    def process_external_msg(self, parameter_list):
+        raise NotImplementedError
+
+    def process_internal_msg(self, payload):
+        print(payload)
+        changed_state = True
+        # TODO: extract streaming state from payload
+        # compare old state against new state
+        # if change then home should send data
+        # raise NotImplementedError
+        if changed_state:
+            self.home.send_msg_2_server(payload)
+            self.clean_count()
+        super().process_internal_msg()
+
+
+class Movement(Device):
+    def process_external_msg(self, parameter_list):
+        raise NotImplementedError
+
+    def process_internal_msg(self, parameter_list):
+        if self.home.lookout:
+            self.home.send_msg_2_server()
+            self.clean_count()
+        else:
+            # TODO: evaluate further actions regarding home functionality (for next versions: rooms, auto mode)
+            pass
+        super().process_internal_msg(parameter_list)
--- gateway-rasp/Home.py
+++ gateway-rasp/Home.py
@@ -1,5 +1,6 @@
-import ExternalComm
-import InternalComm
+# import ExternalComm
+# import InternalComm
+from Device import *
 
 
 class Home:
@@ -11,14 +12,48 @@ class Home:
             flag: used to signal the program termination
             light: state of one of the lights at home. TRUE = on - FALSE = off
     """
+
     def __init__(self):
         """ home constructor
         set initial state in Home class.
         """
         print("New Home created")
         self.lookout = False
-        self.flag = True
         self.light = False
+        self.flag = True
+
+        self.devices = {}
+        self.devices = self.get_devices()
+
+    def get_devices(self):
+        # This structure simulates the initial configuration obtained from a remote server
+        # TODO: implement by consuming a REST service 
+        lv_light = Light(0, "online", "connected", "living room light", self)
+        lv_cam = Camera(1, "online", "connected", "living room camera", self)
+        lv_tmp = Temperature(2, "online", "connected", "living room temperature sensor", self)
+        lv_mov = Movement(3, "online", "connected", "living room PIR sensor", self)
+        devices = {
+            0: lv_light,
+            1: lv_cam,
+            2: lv_tmp,
+            3: lv_mov
+        }
+        return devices
+
+    def process_msg_from_device(self, id, payload):
+        source_device = self.devices.get(id, "invalid")
+        if source_device == "invalid":
+            # TODO: Notify server that gateway receives msg from unknown device
+            raise AssertionError
+        source_device.process_internal_msg(payload)
+
+    def send_msg_2_server(self, msg):
+        #TODO: send message to server
+        raise NotImplementedError
+
+    def send_alive_msg(self, msg):
+        #TODO: send alive message to server
+        raise NotImplementedError
 
     def toggle_lookout(self):
         """ This is the toggle_lookout function
@@ -31,12 +66,12 @@ class Home:
         """
         self.lookout = not self.lookout
         print("new lookout state to " + format(self.lookout))
-        ExternalComm.ExternalComm.send_status(format(self.lookout))
+        '''ExternalComm.ExternalComm.send_status(format(self.lookout))
         if self.lookout:
             InternalComm.ThingsComm.send_conf("pir_conf", "1")
         else:
             InternalComm.ThingsComm.send_conf("pir_conf", "0")
-        return self.lookout
+        return self.lookout'''
 
     def toggle_light(self):
         """ This is the toggle_lookout function
@@ -51,12 +86,12 @@ class Home:
         """
         self.light = not self.light
         print("new light state to " + format(self.light))
-        ExternalComm.ExternalComm.send_light(format(self.light))
-        if self.light:
+        # ExternalComm.ExternalComm.send_light(format(self.light))
+        '''if self.light:
             InternalComm.ThingsComm.send_conf("light", "1")
         else:
             InternalComm.ThingsComm.send_conf("light", "0")
-        return self.light
+        return self.light'''
 
     def get_lookout(self):
         return self.lookout
@@ -77,3 +112,7 @@ class Home:
             When False program must finish
         """
         return self.flag
+
+
+home = Home()
+home.process_msg_from_device(0, "hello")
