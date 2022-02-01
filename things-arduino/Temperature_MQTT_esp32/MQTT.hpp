dc062055517ab935f348a8aebbb9fa151e085645
Danny Munera <dannymunera@gmail.com>
2022-01-31 20:53:56 -0500

    Include Temperature sensor with ESP32 nodeMCU

---
+++ things-arduino/Temperature_MQTT_esp32/MQTT.hpp
@@ -0,0 +1,67 @@
+const char* MQTT_BROKER_ADRESS = "192.168.200.29";
+const uint16_t MQTT_PORT = 1883;
+const char* MQTT_CLIENT_NAME = "ESP8266Client_1";
+
+// Topics
+const char* DEVICE_TYPE = "Light";
+const char* DEVICE_ID = "0";
+
+extern int light_state;
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+void SuscribeMqtt()
+{
+  char topic_sub[100];
+  sprintf(topic_sub,"%s/%s", DEVICE_TYPE, DEVICE_ID);
+  mqttClient.subscribe(topic_sub);
+}
+
+String payload;
+void PublisMqtt(unsigned long data)
+{
+   payload = "";
+   payload = String(data);
+   mqttClient.publish("hello/world", (char*)payload.c_str());
+}
+
+void PublisMqttString(char* topic, char* msg)
+{
+   mqttClient.publish(topic, msg);
+}
+
+String content = "";
+void OnMqttReceived(char* topic, byte* payload, unsigned int length) 
+{
+   Serial.print("Received on ");
+   Serial.print(topic);
+   Serial.print(": ");
+
+   content = "";   
+   for (size_t i = 0; i < length; i++) {
+      content.concat((char)payload[i]);
+   }
+   Serial.print(content);
+   Serial.println();
+
+   if((char)payload[0] == '1') {
+      light_state=1;
+      digitalWrite(BUILTIN_LED, HIGH);
+   }
+   else if ((char)payload[0] == '0') {
+      light_state=0;
+      digitalWrite(BUILTIN_LED, LOW);
+   }
+   else if ((char)payload[0] == '2') { 
+      // Toggle light 
+      if (light_state == 1){
+        light_state=0;
+        digitalWrite(BUILTIN_LED, LOW);
+      }
+      else{
+        light_state=1;
+        digitalWrite(BUILTIN_LED, HIGH);
+      }
+  }
+}
---
+++ things-arduino/Temperature_MQTT_esp32/config.h
@@ -0,0 +1,7 @@
+const char* ssid     = "Nett1";
+const char* password = "zvwq1218";
+const char* hostname = "ESP32_1";
+
+IPAddress ip(192, 168, 200, 10);
+IPAddress gateway(192, 168, 200, 7);
+IPAddress subnet(255, 255, 255, 0);
